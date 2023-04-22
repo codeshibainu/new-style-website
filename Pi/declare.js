dd5f306ead994cbe1aa0e3d2414b6e54139e9fe6
Celine10811020 <shinecloudhill@gmail.com>
2023-04-22 21:25:20 +0800

    Add from atom

---
+++ Pi/declare.js
@@ -0,0 +1,23 @@
+var Count;
+var Sentence = [];
+
+var Title;
+var ButtonStart;
+var ButtonOne;
+var ButtonTwo;
+var ButtonThree;
+var ButtonFour;
+var SentenceOne;
+var SentenceTwo;
+var SentenceThree;
+var SentenceNow;
+
+var RandomNumber;
+var SuccessiveNumber;
+var ErrorNumber;
+var Answer;
+var Start;
+var Correct;
+var Successive;
+var SuccessiveMax;
+var Error;
