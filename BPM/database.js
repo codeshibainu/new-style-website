265545c036194273e67fc97b55488de63e2666fe
Celine10811020 <shinecloudhill@gmail.com>
2023-04-23 15:07:46 +0800

    Add from atom

---
+++ BPM/database.js
@@ -0,0 +1,42 @@
+function initialization()
+{
+  Sentence[0] = "ㄅ";
+  Sentence[1] = "ㄆ";
+  Sentence[2] = "ㄇ";
+  Sentence[3] = "ㄈ";
+  Sentence[4] = "ㄉ";
+  Sentence[5] = "ㄊ";
+  Sentence[6] = "ㄋ";
+  Sentence[7] = "ㄌ";
+  Sentence[8] = "ㄍ";
+  Sentence[9] = "ㄎ";
+  Sentence[10] = "ㄏ";
+  Sentence[11] = "ㄐ";
+  Sentence[12] = "ㄑ";
+  Sentence[13] = "ㄒ";
+  Sentence[14] = "ㄓ";
+  Sentence[15] = "ㄔ";
+  Sentence[16] = "ㄕ";
+  Sentence[17] = "ㄖ";
+  Sentence[18] = "ㄗ";
+  Sentence[19] = "ㄘ";
+  Sentence[20] = "ㄙ";
+  Sentence[21] = "ㄧ";
+  Sentence[22] = "ㄨ";
+  Sentence[23] = "ㄩ";
+  Sentence[24] = "ㄚ";
+  Sentence[25] = "ㄛ";
+  Sentence[26] = "ㄜ";
+  Sentence[27] = "ㄝ";
+  Sentence[28] = "ㄞ";
+  Sentence[29] = "ㄟ";
+  Sentence[30] = "ㄠ";
+  Sentence[31] = "ㄡ";
+  Sentence[32] = "ㄢ";
+  Sentence[33] = "ㄣ";
+  Sentence[34] = "ㄤ";
+  Sentence[35] = "ㄥ";
+  Sentence[36] = "ㄦ";
+
+  Count = 37;
+}
