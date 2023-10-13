ce3049d6618f354a2b4d1ba36220d496e2cf17c4
Celine10811020 <shinecloudhill@gmail.com>
2023-10-13 18:59:22 +0800

    add from git push

---
+++ SongShiOne/sortText.py
@@ -0,0 +1,19 @@
+import re
+
+# 打开原始文件
+with open('SongShi.txt', 'r', encoding='utf-8') as file:
+    # 读取文件内容
+    content = file.read()
+
+# 替换标点符号和数字为换行符
+for char in ' 、「」：；（）.『』，。！？0123456789':
+    content = content.replace(char, '\n')
+
+# 删除多余的换行符
+content = re.sub(r'\n+', '\n', content)
+
+# 写入新文件
+with open('clean_SongShi.txt', 'w', encoding='utf-8') as file:
+    file.write(content)
+
+print("处理完成，结果已写入clean_SongShi.txt文件。")
---
+++ ZhongYong/declare.js
@@ -0,0 +1,25 @@
+var Count;
+var Sentence = [];
+
+var Title;
+var ButtonStart;
+var ButtonOne;
+var ButtonTwo;
+var ButtonThree;
+var ButtonFour;
+var ButtonRestart;
+var ButtonOthers;
+var SentenceOne;
+var SentenceTwo;
+var SentenceThree;
+var SentenceNow;
+
+var SentenceRandom;
+var SuccessiveNumber;
+var ErrorNumber;
+var Answer;
+var Start;
+var Correct;
+var Successive;
+var SuccessiveMax;
+var Error;
